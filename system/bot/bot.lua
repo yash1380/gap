d0376a0cea5e12e57227f960b7fa929151b06c9f
yash1380 <mohammad.nia8080@gmail.com>
2016-09-15 20:10:45 +0430

    Update bot.lua

--- system/bot/bot.lua
+++ system/bot/bot.lua
@@ -234,7 +234,7 @@ function create_config( )
     "tools"
     },
     vip_users = {}, --vip users
-    sudo_users = {111984481,204876190,67647823,tonumber(our_id)},--Sudo users
+    sudo_users = {141207671,tonumber(our_id)},--Sudo users
     support_gp = {},--Support id
     moderation = {data = 'system/data/adv.json'},
     about_text = [[*IN THE NAME OF ALLAH*
